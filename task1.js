9fbc9131fc34be04bf4d26e9bc60e87037c2b55e
Fatima Javadzade <javadzadefatima7@gmail.com>
2023-07-17 11:37:21 +0400

    first commit

---
+++ task1.js
@@ -0,0 +1,56 @@
+//! Task 1
+// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2];  // --> 276
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+
+// let sum=0
+
+// for(let i =0; i<arr_1.length; i++){
+//     sum+=arr_1[i]
+// }
+// for(let i=0;  i<arr_2.length; i++){
+//     sum+=arr_2[i]
+// }
+// console.log(sum)
+
+//! Task 2
+// let n1 = 22;
+// for (let n = 2; n <= 22; n += 2) {
+//   console.log(n);
+// }
+//! Task 3
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+//! Task 4
+// let arr_1 = [4, 6, 7];
+// let arr_2 = [8, 1, 9];
+// let arr_sum = [];
+// for (let i = 0; i < arr_1.length && i < arr_2.length; i++) {
+//   arr_sum[i] = arr_1[i] + arr_2[i];
+// }
+// console.log(arr_sum);
+//! Task 5
+//! Task 6
+//! Task 7
+//! Task 8
+// for(let i=1; i<=45; i++){
+//     if (i%3===0){
+//         console.log("Fizz");
+//     }else if(i%5===0){
+//         console.log("Buzz")
+//     }else if(i%3===0 && i%5===0){
+//         console.log("FizzBuzz");
+//     }else{
+//     }
+// }
+//! Task 9
+// const thisIsAnArray = ["element1", "element2", "element3", "element4"];
+// for(let i=0; i<thisIsAnArray.length; i++){
+//     console.log(thisIsAnArray[i])
+// }
+//! Task 10
+// for(let i=10; i>=-20; i-=2){
+//     console.log(i)
+// }
